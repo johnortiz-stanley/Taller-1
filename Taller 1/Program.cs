2ecf681279418498a8741ab616231e4ec57a8370
johnortiz-stanley <john.ortiz@tecnologicoliceocristiano.edu.ec>
2026-07-15 11:46:10 -0500

    Actualizar Readme

--- Taller 1/Program.cs
+++ Taller 1/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Taller_1.Desarrollo;
 
-Jugador objJugador1 = new Jugador("Piero Hincapié", 24, 4, "Defensa");
+Jugador objJugador1 = new Jugador("Moises Caicedo", 19, 4, "Defensa");
 
-Jugador objJugador2 = new Jugador("Enner Valencia", 32, 4, "Delantero");
+Jugador objJugador2 = new Jugador("Willian Pacho", 32, 11, "Delantero");
 
-Equipo objEquipo1 = new Equipo("Emelec", "Guayaquil");
+Equipo objEquipo1 = new Equipo("Barcelona", "Guayaquil");
 
 objEquipo1.AgregarJugador(objJugador1);
 objEquipo1.AgregarJugador(objJugador2);
@@ -13,13 +13,13 @@ objEquipo1.AgregarJugador(objJugador2);
 
 objEquipo1.ListarPlantilla();
 
-Jugador objJugador3 = new Jugador("Moiséc Caicedo", 23, 5, "Medio Campo");
+Jugador objJugador3 = new Jugador("Gabriel Cortes", 28, 7, "Medio Campo");
 
 
-Jugador objJugador4 = new Jugador("Neiser Reascos", 45, 24, "Lateral");
+Jugador objJugador4 = new Jugador("Damian Diaz", 20, 8, "Lateral");
 
 
-Equipo objEquipo2 = new Equipo("Barcelona", "Guayaquil");
+Equipo objEquipo2 = new Equipo("Liga de quito", "Quito");
 
 objEquipo2.AgregarJugador(objJugador3);
 objEquipo2.AgregarJugador(objJugador4);
